b52d46446437a6ca2d849311917f3dd2968f9512
danielar92 <danielar92@gmail.com>
2014-02-10 19:17:02 -0430

    Arreglados los nombres del cvs

--- hoi/hoi/apps/registry/admin.py
+++ hoi/hoi/apps/registry/admin.py
@@ -30,17 +30,67 @@ class PersonaAdmin(admin.ModelAdmin):
 
         ])
         for obj in queryset:
+            if obj.sexo == 0:
+                sexo = 'Femenino'
+            else:
+                sexo ='Masculino'
+
+            if obj.area == 0:
+                area = 'Ortopedia Infantil'
+            elif obj.area == 1:
+                area = 'Hombro'
+            elif obj.area == 2:
+                area = 'Columna'
+            elif obj.area == 3:
+                area = 'Cadera'
+            elif obj.area == 4:
+                area = 'Miembros Inferiores'
+            elif obj.area == 5:
+                area = 'Neuro-Ortopedia'
+            elif obj.area == 6:
+                area = 'Medicina fisica y rehabilitacion'
+            elif obj.area ==7:
+                area = 'Cirugia de mano'
+            elif obj.area == 8:
+                area = 'Pediatria'
+            elif obj.area == 9:
+                area = 'Fisioterapia'
+            elif obj.area == 10:
+                area = 'Terapia del Lenguaje'
+            elif obj.area == 11:
+                area = 'Taller de Ortopedia'
+            elif obj.area == 12:
+                area = 'Terapia Ocupacional'
+
+            if obj.tipo == 0:
+                tipo ='Pasantia'
+            elif obj.tipo == 1:
+                'Post-Grado'
+            elif obj.tipo == 2:
+                'Fellow'
+            elif obj.tipo == 3:
+                'Visitor'
+
+            if obj.estado_civil == 0:
+                tipo ='Soltero'
+            elif obj.estado_civil == 1:
+                'Casado'
+            elif obj.estado_civil == 2:
+                'Divorciado'
+            elif obj.estado_civil == 3:
+                'Viudo'
+
             writer.writerow([
-                smart_str(obj.tipo),
-                smart_str(obj.area),
+                smart_str(tipo),
+                smart_str(area),
                 smart_str(obj.apellidos),
                 smart_str(obj.nombres),
                 smart_str(obj.didentificacion),
                 smart_str(obj.nacionalidad),
-                smart_str(obj.sexo),
+                smart_str(sexo),
                 smart_str(obj.fecha_inicio),
                 smart_str(obj.fecha_fin),
-                smart_str(obj.estado_civil),
+                smart_str(estado_civil),
                 smart_str(obj.estado_nacimiento),
                 smart_str(obj.pais_nacimiento),
             ])
