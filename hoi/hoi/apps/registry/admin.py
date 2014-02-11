2852215f7a4291acdd1837e9dddad293b2ea2af2
danielar92 <danielar92@gmail.com>
2014-02-10 20:00:24 -0430

    Excel esta medio listo, solo tiene un snafu

--- hoi/hoi/apps/registry/admin.py
+++ hoi/hoi/apps/registry/admin.py
@@ -14,19 +14,19 @@ class PersonaAdmin(admin.ModelAdmin):
         writer = csv.writer(response, csv.excel)
         response.write(u'\ufeff'.encode('utf8'))
         writer.writerow([
-            smart_str(u"tipo"),
-            smart_str(u"area"),
-            smart_str(u"apellidos"),
-            smart_str(u"nombres"),
-            smart_str(u"doc_identificacion"),
-            smart_str(u"nacionalidad"),
-            smart_str(u"sexo"),
-            smart_str(u"fecha_inicio"),
-            smart_str(u"fecha_fin"),
-            smart_str(u"fecha_nacimiento"),
-            smart_str(u"estado_civil"),
-            smart_str(u"estado_nacimiento"),
-            smart_str(u"pais_nacimiento"),
+            smart_str(u"Tipo"),
+            smart_str(u"Area"),
+            smart_str(u"Apellidos"),
+            smart_str(u"Nombres"),
+            smart_str(u"Documento Identificacion"),
+            smart_str(u"Nacionalidad"),
+            smart_str(u"Sexo"),
+            smart_str(u"Fecha Inicio"),
+            smart_str(u"Fecha Fin"),
+            smart_str(u"Fecha Nacimiento"),
+            smart_str(u"Estado Civil"),
+            smart_str(u"Estado Nacimiento"),
+            smart_str(u"Pais Nacimiento"),
 
         ])
         for obj in queryset:
@@ -108,9 +108,19 @@ class PersonaAdmin(admin.ModelAdmin):
         row_num = 0
 
         columns = [
-            (u"tipo", 2000),
-            (u"area", 6000),
-            (u"apellidos", 8000),
+            (u"Tipo",2000),
+            (u"Area",2000),
+            (u"Apellidos",2500),
+            (u"Nombres", 2000),
+            (u"Documento Identificacion", 4000),
+            (u"Nacionalidad", 3000),
+            (u"Sexo", 2000),
+            (u"Fecha Inicio", 2000),
+            (u"Fecha Fin", 2000),
+            (u"Fecha Nacimiento", 3000),
+            (u"Estado Civil", 2000),
+            (u"Estado Nacimiento", 3000),
+            (u"Pais Nacimiento", 3000),
         ]
 
         font_style = xlwt.XFStyle()
@@ -125,11 +135,71 @@ class PersonaAdmin(admin.ModelAdmin):
             font_style.alignment.wrap = 1
 
         for obj in queryset:
+            if obj.sexo == 0:
+                sexo = 'Femenino'
+            else:
+                sexo ='Masculino'
+
+            if obj.area == 0:
+                area = 'Ortopedia Infantil'
+            elif obj.area == 1:
+                area = 'Hombro'
+            elif obj.area == 2:
+                area = 'Columna'
+            elif obj.area == 3:
+                area = 'Cadera'
+            elif obj.area == 4:
+                area = 'Miembros Inferiores'
+            elif obj.area == 5:
+                area = 'Neuro-Ortopedia'
+            elif obj.area == 6:
+                area = 'Medicina fisica y rehabilitacion'
+            elif obj.area ==7:
+                area = 'Cirugia de mano'
+            elif obj.area == 8:
+                area = 'Pediatria'
+            elif obj.area == 9:
+                area = 'Fisioterapia'
+            elif obj.area == 10:
+                area = 'Terapia del Lenguaje'
+            elif obj.area == 11:
+                area = 'Taller de Ortopedia'
+            elif obj.area == 12:
+                area = 'Terapia Ocupacional'
+
+            if obj.tipo == 0:
+                tipo ='Pasantia'
+            elif obj.tipo == 1:
+                'Post-Grado'
+            elif obj.tipo == 2:
+                'Fellow'
+            elif obj.tipo == 3:
+                'Visitor'
+
+            if obj.estado_civil == 0:
+                estado_civil ='Soltero'
+            elif obj.estado_civil == 1:
+                estado_civil = 'Casado'
+            elif obj.estado_civil == 2:
+                estado_civil= 'Divorciado'
+            elif obj.estado_civil == 3:
+                estado_civil= 'Viudo'
+
             row_num += 1
             row = [
-                obj.tipo,
-                obj.area,
+                tipo,
+                area,
                 obj.apellidos,
+                obj.nombres,
+                obj.didentificacion,
+                obj.nacionalidad,
+                sexo,
+                obj.fecha_inicio,
+                obj.fecha_fin,
+                obj.fecha_nacimiento,
+                estado_civil,
+                obj.estado_nacimiento,
+                obj.pais_nacimiento
             ]
         for col_num in xrange(len(row)):
             ws.write(row_num, col_num, row[col_num], font_style)
